a0c552cbe2a860ac1c1779731f77cd10b35b01ea
David Morrow <dmorrow@newrelic.com>
2013-02-10 13:39:52 -0800

    Adding minfied json to rake task as well

--- rakefile.rb
+++ rakefile.rb
@@ -6,19 +6,11 @@ namespace :convert do
   task :to_json do
     csv_file  = File.read("data/stats.csv")
     json_file = File.open "data/stats.json", "w"
+    json_min  = File.open "data/stats.min.json", "w"
     data      = CSV.parse(csv_file)
 
-    # puts JSON.parse(data.to_json)
-    # keys = data.first
-    # data.each_with_index do |data_index, row|
-    #   hash = {}
-    #   row.each_with_index do |index, value|
-    #     hash[keys[index]] = value
-    #   end
-    #   data[data_index] = hash
-    # end
-
     json_file.write JSON.pretty_generate(data)
+    json_min.write data.to_json
     puts "converted stats from CSV to JSON format"
   end
 end
