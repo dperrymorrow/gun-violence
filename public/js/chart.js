adf8a9636dc874b618b9ed32d36d19410fb98dbb
David Morrow <dmorrow@newrelic.com>
2013-02-11 13:21:27 -0800

    got initial chart rendering

--- public/js/chart.js
+++ public/js/chart.js
@@ -2,7 +2,22 @@
   "use strict";
   window.GV = window.GV || {};
   window.GV.chart = {
-    variable: "",
+    chartContainer: {},
+    dataSource: {},
+    labels: [],
+    entireUS: [],
+    keys: {
+      state: 0,
+      abbr: 1,
+      totalMurders: 2,
+      gunKill2011: 3,
+      gunKill2010: 4,
+      change: 5,
+      gunKillPerAll: 6,
+      gunKillPerCap: 7,
+      gunRobbPerCap: 8,
+      gunAssaultPerCap: 9
+    },
 
     initialize: function () {
       $.ajax({
@@ -13,7 +28,48 @@
     },
 
     buildChart: function (data) {
-      console.log(data);
+      this.dataSource     = data;
+      this.chartContainer = d3.select("#graph-container");
+      this.labels         = this.dataSource.shift();
+      this.entireUS       = this.dataSource.shift();
+
+      this.display('gunAssaultPerCap');
+    },
+
+    display: function (key) {
+
+      var position = this.keys[key];
+      var values = _.map(this.dataSource, function (data) {
+        return data[position];
+      });
+      var max = _.max(values, function (data) {
+        return parseInt(data, 10);
+      });
+
+      var barHolders = this.chartContainer.selectAll("div")
+        .data(this.dataSource).enter()
+
+        .append("div").attr("class", "bar-container").sort(function (a, b) {
+          return b[position] - a[position];
+        });
+
+      barHolders.append("div").attr("class", "label")
+        .text(function (data, index) {
+          return data[0] + " (" + data[position] + ")";
+        });
+
+      barHolders.append("div").attr("class", "bar")
+        .transition().duration(function (data, index) {
+          return Math.abs(((index + 1) * 0.03) * 1000);
+        })
+        .delay(100)
+        .style("width", function (data) {
+          // console.log("/////")
+          // console.log(max)
+          // console.log(data[position])
+          // console.log( data[position] / max )
+          return (data[position] / max) * 100 + "%";
+        });
     }
 
   };
