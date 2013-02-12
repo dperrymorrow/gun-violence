084c8be75166fddba20e704b5fb58e96f20d1fcf
David Morrow <dmorrow@newrelic.com>
2013-02-11 17:17:20 -0800

    working display with transitions

--- public/js/chart.js
+++ public/js/chart.js
@@ -2,7 +2,11 @@
   "use strict";
   window.GV = window.GV || {};
   window.GV.chart = {
+    currentKey: "",
     chartContainer: {},
+    textLabels: {},
+    barHolders: {},
+    bars: {},
     dataSource: {},
     labels: [],
     entireUS: [],
@@ -22,21 +26,54 @@
     initialize: function () {
       $.ajax({
         type: "get",
-        success: $.proxy(this, 'buildChart'),
+        success: $.proxy(this, 'build'),
         url: "data/stats.min.json"
       });
     },
 
-    buildChart: function (data) {
+    build: function (data) {
       this.dataSource     = data;
       this.chartContainer = d3.select("#graph-container");
       this.labels         = this.dataSource.shift();
       this.entireUS       = this.dataSource.shift();
 
+      this.buildButtons();
+      this.buildChart();
       this.display('gunAssaultPerCap');
     },
 
+    buildButtons: function () {
+      _.each(_.keys(this.keys), _.bind(function (key) {
+        $('#right-col').append("<button class=\"switcher\" data-key=\"" + key + "\">" + this.labels[this.keys[key]] + "</button>");
+      }, this));
+
+      $('#right-col button').click(function () {
+        window.GV.chart.display($(this).data('key'));
+      });
+    },
+
+    buildChart: function () {
+      this.barHolders = this.chartContainer.selectAll("div")
+        .data(this.dataSource).enter()
+        .append("div").attr("class", "bar-container");
+
+
+      this.barHolders.append("div").attr("class", "label")
+        .text(function (data, index) {
+          return data[0];
+        });
+
+      this.barHolders.append("svg").append("rect")
+        .attr("class", "bar").attr("height", "30")
+        .attr("fill", "#9e000c");
+
+      this.bars       = this.chartContainer.selectAll(".bar");
+      this.textLabels = this.chartContainer.selectAll('.label');
+    },
+
     display: function (key) {
+
+      this.currentKey = key;
 
       var position = this.keys[key];
       var values = _.map(this.dataSource, function (data) {
@@ -46,29 +83,24 @@
         return parseInt(data, 10);
       });
 
-      var barHolders = this.chartContainer.selectAll("div")
-        .data(this.dataSource).enter()
+      // this.barHolders.sort(function (a, b) {
+      //   return b[position] - a[position];
+      // });
 
-        .append("div").attr("class", "bar-container").sort(function (a, b) {
-          return b[position] - a[position];
-        });
+      this.textLabels.text(function (data, index) {
+        return data[0] + " (" + data[position] + ")";
+      });
 
-      barHolders.append("div").attr("class", "label")
-        .text(function (data, index) {
-          return data[0] + " (" + data[position] + ")";
-        });
-
-      barHolders.append("div").attr("class", "bar")
-        .transition().duration(function (data, index) {
-          return Math.abs(((index + 1) * 0.03) * 1000);
+      this.bars
+        .transition().duration(1000)
+        .delay(function (data, index) {
+          return Math.abs(((index + 1) * 0.03));
         })
-        .delay(100)
-        .style("width", function (data) {
-          // console.log("/////")
-          // console.log(max)
-          // console.log(data[position])
-          // console.log( data[position] / max )
+        .attr("width", function (data) {
           return (data[position] / max) * 100 + "%";
+        })
+        .attr("opacity", function (data) {
+          return (data[position] / max) + 0.09;
         });
     }
 
